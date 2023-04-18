51054720f7b45acf81094b682530ef7c3f0c3676
vvcaw <eric.elflein@googlemail.com>
2023-04-18 20:34:09 +0200

    fixup devenv.nix to use cmake / emcmake for building

--- devenv.nix
+++ devenv.nix
@@ -12,9 +12,16 @@
     pkgs.lld_14
   ];
 
+  # Compile commands are only exported correctly if you don't use emcmake
+  scripts.setup.exec = ''
+    export EM_CACHE=$DEVENV_ROOT/.emscripten_cache
+    mkdir -p $DEVENV_ROOT/build
+    cmake -B build -S $DEVENV_ROOT -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+  '';
+
   scripts.build.exec = ''
-    export EM_CACHE=`pwd`/.emscripten_cache
-    emcc main.cpp -o build/index.html -s USE_SDL=2
+    emcmake cmake -B build -S $DEVENV_ROOT
+    make -C $DEVENV_ROOT/build
   '';
 
   scripts.serve.exec = ''
